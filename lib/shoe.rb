4d29079327e7ce2f77d7343f76d8643246ef9b4a
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-18 18:47:56 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-
+  BRANDS
   def initialize(brand)
     @brand = brand
   end
@@ -11,4 +11,25 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+end
+
+class Book
+  attr_accessor :author, :page_count  # remove the attr_accessor for genre
+  attr_reader :title, :genre  # add an attr_reader for genre
+ 
+  GENRES = []
+ 
+  def initialize(title)
+    @title = title
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+  # create the writer for genre and add the logic for the class constant
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre 
+  end
 end
