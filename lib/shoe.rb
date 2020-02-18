24b93830a31a978b733d6775ab83fe03e518cf51
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-18 18:49:05 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,19 +1,35 @@
-
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   BRANDS = []
-   def initialize(brand)
-     @brand = brand
-     brands = @brand
-
-        if  !(BRANDS.include?(brands)) then
-          BRANDS << brands
-        end
-  
+  def initialize(brand)
+    @brand = brand
   end
+
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
+  end
+
+end
+
+class Book
+  attr_accessor :author, :page_count  # remove the attr_accessor for genre
+  attr_reader :title, :genre  # add an attr_reader for genre
+ 
+  GENRES = []
+ 
+  def initialize(title)
+    @title = title
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+  # create the writer for genre and add the logic for the class constant
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre 
   end
 end
